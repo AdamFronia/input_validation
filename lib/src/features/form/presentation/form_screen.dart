8e894088b7fd54d1a1106a20dce5d5a4cbaddbcf
AdamFronia <166802660+AdamFronia@users.noreply.github.com>
2024-05-23 11:01:04 +0200

    task

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -30,6 +30,7 @@ class FormScreen extends StatelessWidget {
                 label: Text("Passwort"),
               ),
               autovalidateMode: AutovalidateMode.onUserInteraction,
+              obscureText: true,
               validator: validatePw,
             ),
             const SizedBox(height: 32),
@@ -44,12 +45,28 @@ class FormScreen extends StatelessWidget {
   }
 
   String? validateEmail(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Email darf nicht leer sein';
+    }
+    if (input.length <= 5) {
+      return 'Email muss mehr als 5 Zeichen haben';
+    }
+    if (!input.contains('@')) {
+      return 'Email muss ein "@" Zeichen enthalten';
+    }
+    if (!(input.endsWith('.com') || input.endsWith('.de'))) {
+      return 'Email muss mit ".com" oder ".de" enden';
+    }
     return null;
   }
 
   String? validatePw(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    if (input == null || input.isEmpty) {
+      return 'Passwort darf nicht leer sein';
+    }
+    if (input.length < 6 || input.length > 12) {
+      return 'Passwort muss zwischen 6 und 12 Zeichen lang sein';
+    }
     return null;
   }
 }
